776adf1b0caefe0a8c855a77e73ab44ac6c032c6
Victor Padron Garcia <vpg66@alu.ua.es>
2024-03-30 12:45:03 +0100

    Cambioado a toggles, faltaria arreglar un bug y añadir boton siguiente

---
+++ Assets/MenuInicio/BotonesScripts/Button.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "Button", menuName = "Button", order = 0)]
+public class Buttons : ScriptableObject {
+    public GameObject button;
+    public Sprite imagen;
+}
